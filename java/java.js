05670b150ce5b4dab09a0c49811b01a3063181a5
sgidan <sarahgidan@gmail.com>
2019-06-24 09:54:31 -0700

    image function for giphs & search form

--- java/java.js
+++ java/java.js
@@ -14,30 +14,70 @@ function renderButtons() {
         a.attr("data-name", topics[i]);
         a.text(topics[i]);
         $("#btn").append(a);
+    };
+    $(".animal").on("click", function (event) {
+        event.preventDefault();
+        displayGif(this);
     }
+    )
 
-}   
+};
+
+$(document).ready(function () {
     renderButtons();
 
-    $(document).on("click", ".btn", dipsplayGif);
+});
+
+
+// "click", ".btn", dipsplayGif());
 
 
 
-function dipsplayGif() {
-    https://api.giphy.com/v1/gifs/search?api_key=8IU6p9tovrWHktCTRmpFupLJcR4hpEi3&q=animals&limit=25&offset=0&rating=G&lang=en
+function displayGif(button) {
 
-    var animal = $(".animal").attr("data-name",dipsplayGif);
+    console.log("this click listener is active! ");
+
+    var animal = $(button).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        animal + "api_key=8IU6p9tovrWHktCTRmpFupLJcR4hpEi3&q=animals&limit=25&offset=0&rating=G&lang=en";
+        animal + "&api_key=8IU6p9tovrWHktCTRmpFupLJcR4hpEi3&limit=25";
+
+    console.log(animal);
 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .then(function (response) {
-            $("#gif-here").text(JSON.stringify(response));
             console.log(response)
+
+            var results = response.data;
+
+            results.forEach(function (element) {
+                var gifDiv = $('<div>');
+                var animalImage = $('<img>');
+                var rating = results.rating
+                var p = $('<p>').text('Rating: ' + rating);
+                animalImage.attr('src', element.images.fixed_height.url);
+                gifDiv.append(p);
+                gifDiv.append(animalImage);
+                $('#gif-here').prepend(gifDiv);
+
+            })
+
         });
-       
-        
-}       
+
+
+}
+$("#add-animal").on("click", function (event) {
+    event.preventDefault();
+    // This line of code will grab the input from the textbox
+    var animal = $("#animal-input").val().trim();
+
+    // The movie from the textbox is then added to our array
+    topics.push(animal);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+    
+});
+
